8e31aac4899d0e0acc4377348c0e6dd12314273c
Ricky Greig <78995684+Rickygreig@users.noreply.github.com>
2021-05-17 15:37:18 -0400

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,93 @@
+let userScore = 0;
+let computerScore= 0;
+const userScore_span = document.getElementById("userScore");
+const computerScore_span = document.getElementById("computerScore");
+const scoreBoard_div = document.querySelector(".scoreBoard");
+const result_p = document.querySelector(".result p");
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random()*3));
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML= userScore;
+    computerScore_span.innerHTML= computerScore;
+    result_p.innerHTML= `Your ${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}, you win!`
+    document.getElementById(userChoice).classList.add('green-glow');
+    document.getElementById('divId').classList.add('green-glow');
+    setTimeout(function () {document.getElementById('divId').classList.remove('green-glow')}, 300)
+    setTimeout(function () {document.getElementById(userChoice).classList.remove('green-glow')}, 300)
+}
+
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore.innerHTML= userScore;
+    computerScore_span.innerHTML= computerScore;
+    result_p.innerHTML= `${convertToWord(computerChoice)} beats your ${convertToWord(userChoice)}, you lose!`
+    document.getElementById(userChoice).classList.add('red-glow');
+    document.getElementById('divId').classList.add('red-glow');
+    setTimeout(function() {document.getElementById('divId').classList.remove('red-glow')}, 300)
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('red-glow')}, 300)
+}
+
+function draw(computerChoice, userChoice){
+    userScore.innerHTML= userScore;
+    computerScore_span.innerHTML= computerScore;
+    result_p.innerHTML= convertToWord(computerChoice) +" V " + convertToWord(userChoice) + ", draw!"
+    document.getElementById(userChoice).classList.add('gray-glow');
+    document.getElementById('divId').classList.add('gray-glow');
+    setTimeout(function() {document.getElementById('divId').classList.remove('gray-glow')}, 300)
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('gray-glow')}, 300)
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+
+function main() {
+rock_div.addEventListener('click', function() {
+  game('r');
+})
+
+paper_div.addEventListener('click', function() {
+    game('p');
+})
+
+scissors_div.addEventListener('click', function() {
+    game('s');
+})
+}
+
+
+main();
